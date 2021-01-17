4d50031356f10679573c12852950111d4b1caed9
galla-okto <galla.okto1@gmail.com>
2021-01-17 11:53:46 +0200

    Добавляет модель Schedule

--- mainapp/models.py
+++ mainapp/models.py
@@ -16,5 +16,21 @@ class Workout(models.Model):
     def __str__(self):
         return self.name
 
+class Schedule(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.PROTECT)
+    trainer = models.ForeignKey(Trainer, on_delete=models.PROTECT)
+
+    date_start = models.DateField()
+    date_end = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+
+    def __str__(self):
+        return self.workout
+
+
+
+
+
 
 
