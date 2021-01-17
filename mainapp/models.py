7833c8cac2bbaa50216cae3a13d99710fc1961ae
galla-okto <galla.okto1@gmail.com>
2021-01-17 11:44:16 +0200

    Добавляет модель Trainer

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class Trainer(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField(unique=True)
+    data = models.TextField(max_length=200)
+
+    @property
+    def __str__(self):
+        return self.name
+
