d8ecdd68f1c33313708d2c02d885e644d0b866d1
galla-okto <galla.okto1@gmail.com>
2021-01-17 14:08:55 +0200

    Добавляет новое поле в Trainer и добавляет его админку

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Trainer, Workout, Schedule
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'position', 'email'
+    list_display_links = 'name',
--- mainapp/models.py
+++ mainapp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Trainer(models.Model):
     name = models.CharField(max_length=80)
     email = models.EmailField(unique=True)
-    data = models.TextField(max_length=200)
+    position = models.CharField(max_length=200)
+    data = models.TextField(max_length=400)
 
-    @property
     def __str__(self):
         return self.name
 
@@ -20,13 +20,14 @@ class Schedule(models.Model):
     workout = models.ForeignKey(Workout, on_delete=models.PROTECT)
     trainer = models.ForeignKey(Trainer, on_delete=models.PROTECT)
 
+    title = models.CharField(max_length=80)
     date_start = models.DateField()
     date_end = models.DateField()
     time_start = models.TimeField()
     time_end = models.TimeField()
 
     def __str__(self):
-        return self.workout
+        return self.title
 
 
 
