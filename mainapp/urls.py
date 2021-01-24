2713c302c41b513210206cf6351c0a5aaa8cd9f3
galla-okto <galla.okto1@gmail.com>
2021-01-24 16:28:21 +0200

    Добавляет страницу для занятия

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,4 +7,5 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('workout/<int:pk>/', views.workout, name='workout'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from mainapp.models import Workout
 
@@ -11,5 +11,19 @@ def index(request):
     }
     return render(request, 'mainapp/index.html', context)
 
+
 def about(request):
     return render(request, 'mainapp/about.html')
+
+
+def workout(request, pk):
+    workout = get_object_or_404(Workout, pk=pk)
+    # try:
+    #     workout = Workout.objects.get(pk=pk)
+    # except Exception as e:
+    #     return
+    # workout = Workout.objects.filter(pk=pk).first()
+    context = {
+        'workout': workout
+    }
+    return render(request, 'mainapp/workout.html', context)
