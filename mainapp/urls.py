b531a7c84a0ddfb0c785473a913931166a73d245
galla-okto <galla.okto1@gmail.com>
2021-02-06 11:59:07 +0200

    Удаляет неиспользуемый код

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,17 +6,14 @@ import mainapp.views as views
 app_name = 'mainapp'
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('',
          views.WorkoutListView.as_view(),
          name='index'),
 
-    # path('about/', views.about, name='about'),
     path('about/',
          TemplateView.as_view(template_name='mainapp/about.html'),
          name='about'),
 
-    # path('workout/<int:workout_pk>/', views.workout, name='workout'),
     path('workout/<int:pk>/',
          views.WorkoutDetailView.as_view(),
          name='workout'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
@@ -14,65 +13,25 @@ class PageNameMixin:
         return context
 
 
-# def index(request):
-#     workouts = Workout.objects.all()
-#     context = {
-#         'page_name': 'главная',
-#         'workouts': workouts,
-#     }
-#     return render(request, 'mainapp/index.html', context)
-
-
 class WorkoutListView(PageNameMixin, ListView):
     model = Workout
     page_name = 'тренировки'
-    # paginate_by = 15
-
-# def about(request):
-#     return render(request, 'mainapp/about.html')
-
-
-# def workout(request, workout_pk):
-#     workout = get_object_or_404(Workout, pk=pk)
-#     # try:
-#     #     workout = Workout.objects.get(pk=pk)
-#     # except Exception as e:
-#     #     return
-#     # workout = Workout.objects.filter(pk=pk).first()
-#     context = {
-#         'workout': workout
-#     }
-#     return render(request, 'mainapp/workout.html', context)
-
-
-# class WorkoutDetailView(DetailView):
-#     model = Workout
-#     template_name = 'mainapp/workout.html'
-#     pk_url_kwarg = 'workout_pk'
-#     context_object_name = 'workout'
 
 
 class WorkoutDetailView(PageNameMixin, DetailView):
     model = Workout
     page_name = 'тренировка'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['page_name'] = 'тренировка'
-    #     return context
-
 
 class WorkoutCreateView(CreateView):
     model = Workout
     fields = '__all__'
-    # success_url = reverse_lazy('mainapp:index')
 
 
 class WorkoutUpdateView(UpdateView):
     model = Workout
     fields = '__all__'
     template_name_suffix = '_update_form'
-    # success_url = reverse_lazy('mainapp:index')
 
 
 class WorkoutDeleteView(DeleteView):
