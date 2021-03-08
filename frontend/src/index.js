ff808df498494247b0137c540f52cd342f4a1bc1
galla-okto <galla.okto1@gmail.com>
2021-03-08 21:47:38 +0200

    Выводит в консоль данные о клиентах и курсах

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,13 @@
-alert(1);
+import axios from "axios";
+const url = "http://127.0.0.1:8000/myapi/api/clients/";
+
+axios({
+    method: "get",
+    url
+  }).then((response) => console.log(response.data));
+
+const url_workouts = "http://127.0.0.1:8000/myapi/api/workouts/";
+
+fetch(url_workouts)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
