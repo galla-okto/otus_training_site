783e2b10ffc0ebd94ccfb76fe64fd185a103c775
galla-okto <galla.okto1@gmail.com>
2021-04-07 17:23:59 +0300

    Выводит все компоненты на главную страницу

--- frontendreact/src/components/main.jsx
+++ frontendreact/src/components/main.jsx
@@ -14,7 +14,7 @@ class Main extends React.Component {
             <br />
             <br />
             <h3>Страница просмотра одного курса</h3>
-            <WorkoutsCard />
+            <WorkoutsCard code={"1"}/>
             <br />
             <br />
             <h3>Страница с возможностью записи на курс</h3>
--- frontendreact/src/components/workouts-card.jsx
+++ frontendreact/src/components/workouts-card.jsx
@@ -3,43 +3,45 @@ import Button from "react-bootstrap/Button";
 
 class WorkoutsCard extends React.Component {
     constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading"
-    };
+        super(props);
+        this.state = {
+          data: [],
+          loaded: false,
+          placeholder: "Loading"
+        };
   }
 
   componentDidMount() {
-    const url_workouts = "http://127.0.0.1:8000/myapi/api/workouts/1";
-    fetch(url_workouts)
-      .then((response) => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
+        const {code} = this.props;
+        const url_workouts = "http://127.0.0.1:8000/myapi/api/workouts/"+code;
+
+        fetch(url_workouts)
+        .then((response) => {
+            if (response.status > 400) {
+              return this.setState(() => {
+                return { placeholder: "Something went wrong!" };
+              });
+            }
+            return response.json();
+          })
+          .then((data) => {
+            this.setState(() => {
+              return {
+                data,
+                loaded: true
+              };
+            });
           });
-        }
-        return response.json();
-      })
-      .then((data) => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true
-          };
-        });
-      });
   }
 
   render() {
-        let workout = this.state.data;
+        const {data} = this.state;
 
         return (
         <div>
-            <h2 className="panel-title">{workout.name} {workout.star_rating}*</h2>
-            <p>Initial leve: {workout.initial_level}</p>
-            <p>{workout.description}</p>
+            <h2 className="panel-title">{data.name} {data.star_rating}*</h2>
+            <p>Initial leve: {data.initial_level}</p>
+            <p>{data.description}</p>
             <Button variant="secondary">Enroll in</Button>
         </div>
     );
